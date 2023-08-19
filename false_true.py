be43fb9aed2337c2bfdc12663c0ffdae31d53671
Your Name <jymohcooler@gmail.com>
2023-08-19 14:04:14 +0300

    added false_true.py file

---
+++ false_true.py
@@ -0,0 +1,24 @@
+def false_true(a,b,c):
+    result = 0
+    
+    if a > 0:
+            result +=1
+    if b> 0:
+            result +=1
+    if c > 0:
+            result +=1
+    print(result == 2 )       
+    return result == 2        
+
+
+
+false_true(2, 4, -3) 
+
+false_true(-4, 6, 8) 
+
+false_true(4, -6, 9) 
+
+false_true(-4, 6, 0)
+
+
+false_true(-14, -3, -4)
